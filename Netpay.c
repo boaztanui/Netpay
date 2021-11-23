641bdd56d3439e8e3c98577886938c81597d8ae3
Boaz Tanui <boaztanui058@gmail.com>
2021-11-23 06:03:16 +0300

    Add project files.

---
+++ Netpay.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+int main()
+{
+    float basicPay, sales, netPay;
+
+    //BASIC PAY
+    printf("Please input your Basic Pay: ");
+    scanf("%f", &basicPay);
+
+    //SALES
+    printf("Please input your Sales: ");
+    scanf("%f", &sales);
+
+    if (sales < 10000) {
+        //here the commission is 0. So no need of even doing that calculations
+        netPay = basicPay;
+        printf("Your Net Pay is %.2f", netPay);
+    }
+    else if (sales >= 20000) {
+        // ((basicPay*8)/100) is where we do the commission
+        netPay = basicPay + ((basicPay * 8) / 100);
+        printf("Your Net Pay is %.2f", netPay);
+
+    }
+    else if (sales >= 50000) {
+        netPay = basicPay + ((basicPay * 12) / 100);
+        printf("Your Net Pay is %.2f", netPay);
+
+    }
+    else {
+        netPay = basicPay + ((basicPay * 17) / 100);
+        printf("Your Net Pay is %.2f", netPay);
+    }
+
+    return 0;
+}
